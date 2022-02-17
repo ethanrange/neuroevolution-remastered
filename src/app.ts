a28765cef9ef1cb0d20e566c6010d79287c11994
Ethan Range <65268454+ethanrange@users.noreply.github.com>
2022-02-17 03:22:35 +0000

    Implement gradient background

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,29 @@
 function setup() {
   console.log("Program initialised");
 
-  createCanvas(windowWidth, windowHeight)
-  rectMode(CENTER).noFill().frameRate(60);
+  createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  frameRate(60);
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function draw() {
-  background(240);
+function drawBackground() {
+  let c1: p5.Color = color(63, 191, 191),  c2: p5.Color = color(200, 230, 255);
 
-  fill(color(50))
-  circle(mouseX, mouseY, 25)
+  for (let y = 0; y < height; y++) {
+    let normY = map(y, 0, height, 0, 1);
+    stroke(lerpColor(c1, c2, normY));
+    line(0, y, width, y);
+  }
+}
+
+function draw() {
+  drawBackground();
+
+  fill(color(50));
+  noStroke();
+  circle(mouseX, mouseY, 25);
 }
